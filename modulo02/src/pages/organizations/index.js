d3617ceffbe097e0a55aba7ffd6c527e1a61c6e5
Ana Carolina Hernandes <anacbhernandes@gmail.com>
2018-08-24 15:34:52 -0300

    updating organizations screen

--- modulo02/src/pages/organizations/index.js
+++ modulo02/src/pages/organizations/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import api from 'services/api';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { View } from 'react-native';
+import { View, Text, AsyncStorage, ActivityIndicator, FlatList } from 'react-native';
+
+import styles from './styles';
 
 export default class Organizations extends Component {
   static navigationOptions = {
@@ -8,7 +11,33 @@ export default class Organizations extends Component {
     tabBarIcon: ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />,
   };
 
+  state = {
+    data: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const response = await api.get(`users/${username}/orgs`);
+
+    this.setState({ data: response.data, loading: false });
+  };
+
+  renderListItem = ({ item }) => <Text>{item.login}</Text>;
+
+  renderList = () => (
+    <FlatList data={this.state.data} keyExtractor={item => item.id.toString()} renderItem={this.renderListItem} />
+  );
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        {this.state.loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
+      </View>
+    );
   }
 }
--- modulo02/src/pages/repositories/index.js
+++ modulo02/src/pages/repositories/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import api from 'services/api';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { View } from 'react-native';
+import { View, AsyncStorage, FlatList, ActivityIndicator } from 'react-native';
+import RepositoryItem from './components/RepositoryItem';
+
+import styles from './styles';
 
 export default class Repositories extends Component {
   static navigationOptions = {
@@ -8,7 +12,33 @@ export default class Repositories extends Component {
     tabBarIcon: ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />,
   };
 
+  state = {
+    data: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.loadRepositories();
+  }
+
+  loadRepositories = async () => {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const response = await api.get(`users/${username}/repos`);
+
+    this.setState({ data: response.data, loading: false });
+  };
+
+  renderListItem = ({ item }) => <RepositoryItem repository={item} />;
+
+  renderList = () => (
+    <FlatList data={this.state.data} keyExtractor={item => item.id.toString()} renderItem={this.renderListItem} />
+  );
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        {this.state.loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
+      </View>
+    );
   }
 }
